038e2708780726d9e1946744e37b2eb7e96933d6
Aleksander <alex-vodon@mail.ru>
2023-01-30 13:08:46 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+        int amount = 20;
+        int miles;
+        miles = cost / amount;
+
+        return miles;
+    }
+}
